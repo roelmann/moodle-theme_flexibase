b6df3ab9cb3a088388e9505b55237fd110e7b989
Richard Oelmann <oelmann.richard@gmail.com>
2015-12-27 10:02:24 +0000

    Issue #4 AwesomeBar - Nav and Admin blocks not hiding per setting

--- lib/awesomelib.php
+++ lib/awesomelib.php
@@ -24,7 +24,7 @@
  *             Essential - Julian Ridden, Gareth Barnard;
  *             Elegance - Julian Ridden, Danny Wahl;
  *             BCU - Jez H, Mike Grant
- *             Decaf - Paul Nichols
+ *             flexibase - Paul Nichols
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -471,5 +471,85 @@ class theme_flexibase_expand_navigation extends global_navigation {
 
     public function get_expandable() {
         return $this->expandable;
+    }
+}
+class theme_flexibase_block_manager extends block_manager {
+
+    /**
+     * Get block contents, with settings/navigation filtered out if applicable.
+     *
+     * @param block_manager $manager The core block manager for this page.
+     * @param core_renderer $output The core renderer for this page.
+     * @param string $region The name of the region to get contents of.
+     */
+    public static function get_filtered_content($manager, $output, $region) {
+        global $USER;
+        $manager->check_is_loaded();
+        $manager->ensure_instances_exist($region);
+
+        $skipsettings = $manager->page->theme->settings->hidesettingsblock || !empty($USER->profile['flexibaseSkipSettingsBlock']);
+        $skipnavigation = $manager->page->theme->settings->hidenavigationblock || !empty($USER->profile['flexibaseSkipNavigationBlock']);
+
+        if (!array_key_exists($region, $manager->visibleblockcontent)) {
+            $blockinstances = array();
+            foreach ($manager->blockinstances[$region] as $block) {
+                // Skip settings and/or navigation blocks as per flexibase theme settings.
+                $skipblock = $block->blockname == 'block_settings' && $skipsettings;
+                $skipblock = $skipblock || ($block->blockname == 'block_navigation' && $skipnavigation);
+                if (!$skipblock) {
+                    $blockinstances[] = $block;
+                }
+            }
+            $contents = array();
+            if (array_key_exists($region, $manager->extracontent)) {
+                $contents = $manager->extracontent[$region];
+            }
+            $contents = array_merge($contents, $manager->create_block_contents($blockinstances, $output, $region));
+            if ($region == $manager->defaultregion) {
+                $addblockui = block_add_block_ui($manager->page, $output);
+                if ($addblockui) {
+                    $contents[] = $addblockui;
+                }
+            }
+            $manager->visibleblockcontent[$region] = $contents;
+        } else {
+            // Block content has already been set, so we may need to remove blocks from the list of visible content.
+            $blocks = array();
+            foreach ($manager->visibleblockcontent[$region] as $block) {
+                if (($skipsettings && strstr($block->attributes['class'], 'block_settings')) ||
+                        ($skipnavigation && strstr($block->attributes['class'], 'block_navigation'))) {
+                    continue;
+                }
+                $blocks[] = $block;
+            }
+            $manager->visibleblockcontent[$region] = $blocks;
+        }
+
+        return $manager->visibleblockcontent[$region];
+    }
+
+    /**
+     * Determine whether a region contains anything. (Either any real blocks, or
+     * the add new block UI.)
+     *
+     * (You may wonder why the $output parameter is required. Unfortunately,
+     * because of the way that blocks work, the only reliable way to find out
+     * if a block will be visible is to get the content for output, and to
+     * get the content, you need a renderer. Fortunately, this is not a
+     * performance problem, because we cache the output that is generated, and
+     * in almost every case where we call region_has_content, we are about to
+     * output the blocks anyway, so we are not doing wasted effort.)
+     *
+     * @param string $region a block region that exists on this page.
+     * @param core_renderer $output a core_renderer. normally the global $OUTPUT.
+     * @return boolean Whether there is anything in this region.
+     */
+    public static function region_has_visible_content($manager, $region, $output) {
+        if (!$manager->is_known_region($region)) {
+            return false;
+        }
+        $content = self::get_filtered_content($manager, $output, $region);
+
+        return $manager->region_has_content($region, $output);
     }
 }
--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -24,7 +24,7 @@
  *             flexibase - Julian Ridden, Gareth Barnard;
  *             Elegance - Julian Ridden, Danny Wahl;
  *             BCU - Jez H, Mike Grant
- *             Decaf - Paul Nichols
+ *             flexibase - Paul Nichols
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -535,6 +535,43 @@ class theme_flexibase_core_renderer extends core_renderer {
             return html_writer::div($contents, 'alert alert-danger', $attributes);
         }
         return parent::box($contents, $classes, $id, $attributes);
+    }
+    /**
+     * Output all the blocks in a particular region.
+     *
+     * @param string $region the name of a region on this page.
+     * @return string the HTML to be output.
+     */
+    public function blocks_for_region($region) {
+        global $USER;
+        $blockswanted = array();
+        $blocks = $this->page->blocks->get_blocks_for_region($region);
+        $blockcontents = theme_flexibase_block_manager::get_filtered_content($this->page->blocks, $this, $region);
+        $lastblock = null;
+        $zones = array();
+        foreach ($blocks as $block) {
+            $zones[] = $block->title;
+        }
+        $output = '';
+
+        foreach ($blockcontents as $bc) {
+            if ($bc instanceof block_contents) {
+                // Skip settings and/or navigation blocks as per flexibase theme settings.
+                $skipsettings = $this->page->theme->settings->hidesettingsblock || !empty($USER->profile['flexibaseSkipSettingsBlock']);
+                $skipnavigation = $this->page->theme->settings->hidenavigationblock || !empty($USER->profile['flexibaseSkipNavigationBlock']);
+                $skipblock = $skipsettings && substr($bc->attributes['class'], 0, 15) == 'block_settings ';
+                $skipblock = $skipblock || ($skipnavigation && substr($bc->attributes['class'], 0, 17) == 'block_navigation ');
+                if (!$skipblock) {
+                    $output .= $this->block($bc, $region);
+                    $lastblock = $bc->title;
+                }
+            } else if ($bc instanceof block_move_target) {
+                $output .= $this->block_move_target($bc, $zones, $lastblock);
+            } else {
+                throw new coding_exception('Unexpected type of thing (' . get_class($bc) . ') found in list of block contents.');
+            }
+        }
+        return $output;
     }
 
 }
