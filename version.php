25373deb73e69cabb001e34ea05ee6c00f9f4688
Richard Oelmann <oelmann.richard@gmail.com>
2016-01-10 18:00:52 +0000

    Bump version

--- version.php
+++ version.php
@@ -31,9 +31,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016010700;
+$plugin->version   = 2016010701;
 $plugin->requires  = 2015051100;
-$plugin->release  = 'v3.0.7';
+$plugin->release  = 'v3.0.7a';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_flexibase';
 $plugin->dependencies = array(
