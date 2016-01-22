75582099b4543dcca5c99a005d3ff8a92e169272
Richard Oelmann <oelmann.richard@gmail.com>
2016-01-22 23:46:04 +0000

    add licence text to new themesettingsnav.php

--- layout/includes/themesettingsnav.php
+++ layout/includes/themesettingsnav.php
@@ -1,4 +1,34 @@
-<h3>Quick Settings Links:</h3>
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Flexibase settings file.
+ *
+ * @package    theme_flexibase
+ * @author     2015 Richard Oelmann
+ * @copyright  2015 R. Oelmann
+ * @copyright  Bootstrap - 2014 Bas Brands
+ *             Essential - Julian Ridden, Gareth Barnard;
+ *             Elegance - Julian Ridden, Danny Wahl;
+ *             BCU - Jez H, Mike Grant
+ *             Decaf - Paul Nichols
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ ?>
+ <h3>Quick Settings Links:</h3>
 <a class="btn btn-success" href="<?php echo $CFG->wwwroot ?>/theme/flexibase/settings/settingshelp.php">Help Docs</a>
 <a class="btn btn-success" href="settings.php?section=theme_flexibase_generic">General</a>
 <a class="btn btn-success" href="settings.php?section=theme_flexibase_lessbrand">Main Branding</a>
