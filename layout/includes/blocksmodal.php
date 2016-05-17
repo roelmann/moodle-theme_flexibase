8a56437cadbd6c26783e9041b5a35e640d777661
roelmann <oelmann.richard@gmail.com>
2016-05-17 21:46:41 +0100

    buttons on page block

--- layout/includes/blocksmodal.php
+++ layout/includes/blocksmodal.php
@@ -8,15 +8,32 @@
                 <h3 class="modal-title"><?php echo $PAGE->title ?></h3>
             </div>
             <div class="modal-body">
-                <div class="modal-topcontent">
+                <div class="modal-usercontent">
                     <?php
                     if (isloggedin() ) {
                         echo $OUTPUT->user_picture($USER, array('link' => false, 'size' => 200));
                     }
+                    ?>
+                    <div class="blockmodallinks">
+                        <?php
+                        echo $OUTPUT->page_heading_button();
+                        $participantslink = $CFG->wwwroot.'/user/index.php?id='.$COURSE->id.'&mode=1';
+                        $dashboardlink = $CFG->wwwroot.'my/index.php';
+                        $messageslink = $CFG->wwwroot.'message/index.php';
+                        ?>
+                        <a href=<?php echo $participantslink; ?> class="btn btn-success" title="Participants"><i class="fa-users fa fa-fw"></i>Participants</a>
+                        <a href=<?php echo $dashboardlink; ?> class="btn btn-success" title="Dashboard"><i class="fa-dashboard fa fa-fw"></i>My Dashboard</a>
+                        <a href=<?php echo $messageslink; ?> class="btn btn-success" title="Messages"><i class="fa-envelope fa fa-fw"></i>My Messages</a>
+                    </div>
+                    <?php
                     if ($PAGE->pagelayout == 'course' OR $PAGE->pagelayout == 'incourse') {
                         require('radialcoursecompletion.php');
                         require('radialcoursegrade.php');
                     }
+                    ?>
+                </div>
+                <div class="modal-topcontent">
+					<?php
                     if ($hasside['modaltop']) {
                         echo $OUTPUT->blocks('side-modaltop', "flexcontentmodaltop flexcontainer");
                     }
